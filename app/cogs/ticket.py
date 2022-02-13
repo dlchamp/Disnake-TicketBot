1fce030972db3163446978b1d506bad8750401a7
DLCHAMP <36091350+dlchamp@users.noreply.github.com>
2022-02-13 12:27:06 -0600

    cleaned up on_messge

--- app/cogs/ticket.py
+++ app/cogs/ticket.py
@@ -6,6 +6,7 @@ from disnake.ext import commands
 
 import config
 from utils.db import get_ticket_data, update_ticket_data
+from utils.check import check_attachments, check_message
 
 
 class TicketButton(disnake.ui.View):
@@ -251,83 +252,63 @@ class Ticket(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        help_channel_id = config.HELP_CHANNEL
-        owner_id = config.OWNER_ID
-        attachments = message.attachments
+        help_channel = await self.bot.fetch_channel(config.HELP_CHANNEL)
 
-        # auto delete pinned "new thread" message
-        if message.channel.id == help_channel_id:
-            if message.type == disnake.MessageType.thread_created:
-                await message.delete()
-        """
-        check if message is a DM and if it's from the bot owner.
-        update the embed message in help channel if all requirements are met
+        if message.channel == help_channel:
+            guild = message.guild
+            owner = guild.owner
+            admin_role = guild.get_role(config.ADMIN_ROLE)
+            '''
+            Add new embed with attached "Start Support" button or
+            edit current embed
 
-        Requirements:
-        Message must be a DM from the bot owner ID (set in env)
-        comma separated channel ID, message ID
-        if message is for new message, channel ID must be included after :
-        uploaded sample.json
-        """
-        if isinstance(message.channel, disnake.DMChannel):
-            if message.author.id == owner_id:
-                if message.content.startswith("new"):
-                    try:
-                        channel_id = message.content.split(":")[1].strip()
-                        channel = await self.bot.fetch_channel(channel_id)
-                    except:
-                        await message.channel.send(
-                            'Please make sure your message is in the correct format\nMessage format: "new: channel ID" (upload sample.json), then send'
-                        )
+            Check if the message comes from an Admin role from the guild owner
+            '''
+            if (
+                message.author == owner or
+                admin_role in message.author.roles
+                ):
 
-                    if len(attachments) == 1:
-                        if attachments[0].filename.endswith("json"):
-                            url = attachments[0].url
-                            response = requests.get(url).json()
-                            embed_dict = response["embed"]
+                '''
+                Add new embed to the channel - only attach file with an empty message
+                file must be the edited sample.json file
+                '''
+                if message.content == '':
+                    '''
+                    Message content is empty - new embed
+                    check for correct file criteria, must be 1 file
+                    must be the sample.json
+                    '''
+                    embed = await check_attachments(message)
 
-                            embed = disnake.Embed.from_dict(embed_dict)
-                            await channel.send(
-                                content=None, embed=embed, view=TicketButton(self.bot)
-                            )
-                        else:
-                            await message.channel.send(
-                                "Please upload only the sample.json file"
-                            )
-                    else:
-                        await message.channel.send(
-                            "Please upload only the sample.json file"
-                        )
+                    if embed == 'Error':
+                        await channel.send('Please check the sample.json for proper formatting.', delete_after=5)
+                        return
+                    await channel.send(embed=embed, view=TicketButton(self.bot))
+                    await message.delete()
 
                 else:
+                    '''
+                    Message content is not empty, should be a message ID only
+                    Check attachments, and check message ID to confirm it's a message
+                    If no errors, update embed in channel
+                    '''
+                    msg = await check_message(message)
+                    embed = await check_attachments(message)
 
-                    try:
-                        message_content = message.content.replace(", ", " ").strip()
-                        channel_id, message_id = message_content.split(" ")
-                        channel = await self.bot.fetch_channel(channel_id)
-                        msg = await channel.fetch_message(message_id)
-                    except:
-                        await message.channel.send(
-                            f"Message should contain the channel ID and message ID in the proper format.\n(Example: 941546832520163328, 941558879744045096 (upload file) then send message."
-                        )
-                        return
-
-                    if len(attachments) == 1:
-                        if attachments[0].filename.endswith("json"):
-                            url = attachments[0].url
-                            response = requests.get(url).json()
-                            embed_dict = response["embed"]
-
-                            embed = disnake.Embed.from_dict(embed_dict)
+                    if not msg == 'Error':
+                        if not embed == 'Error':
                             await msg.edit(content=None, embed=embed)
                         else:
-                            await message.channel.send(
-                                "Please upload only the sample.json file"
-                            )
+                            await channel.send('Please check the sample.json', delete_after=5)
                     else:
-                        await message.channel.send(
-                            "Please upload only the sample.json file"
-                        )
+                        await channel.send('No message with that ID was found.', delete_after=5)
+
+                    await message.delete()
+
+
+
+
 
 
 def setup(bot):
