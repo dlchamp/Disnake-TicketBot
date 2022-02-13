84c1866b96bf4e3ead8f4840d1503633750ed4bc
DLCHAMP <36091350+dlchamp@users.noreply.github.com>
2022-02-13 12:27:13 -0600

    removed owner ID

--- app/config.py
+++ app/config.py
@@ -11,6 +11,5 @@ Update values in .env file
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
 HELP_CHANNEL = int(os.getenv("HELP_CHANNEL"))
-OWNER_ID = int(os.getenv("OWNER_ID"))
 ADMIN_ROLE = int(os.getenv("ADMIN_ROLE"))
 STAFF_ROLE = int(os.getenv("STAFF_ROLE"))
