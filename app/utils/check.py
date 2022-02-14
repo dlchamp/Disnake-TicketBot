f0234dd53a721abff0004cd2830c8dc5c7aed459
DLCHAMP <36091350+dlchamp@users.noreply.github.com>
2022-02-14 13:52:08 -0600

    Update check.py

--- app/utils/check.py
+++ app/utils/check.py
@@ -5,21 +5,24 @@ import asyncio
 
 
 async def check_attachments(message):
-    attachment = message.attachments[0]
-    channel = message.channel
-    if attachment.filename == "sample.json":
-        url = attachment.url
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                response = await response.json()
+    if len(message.attachments) == 1:
+        attachment = message.attachments[0]
+        channel = message.channel
+        if attachment.filename == "sample.json":
+            url = attachment.url
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    response = await response.json()
 
-        embed = response["embed"]
+            embed = response["embed"]
 
-        try:
-            embed = disnake.Embed.from_dict(embed)
-            return embed
-        except:
-            return "Error"
+            try:
+                embed = disnake.Embed.from_dict(embed)
+                return embed
+            except:
+                return "Error"
+
+    return None
 
 
 async def check_message(message):
@@ -30,3 +33,5 @@ async def check_message(message):
         return msg
     except:
         return "Error"
+
+
